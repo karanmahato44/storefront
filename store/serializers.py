fbdd85166e73915753b829ff8c77d90c91c87208
Karan Mahato <44karanmahato@gmail.com>
2023-07-13 13:09:51 +0545

    use model serializers

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,34 @@
+from decimal import Decimal
 from rest_framework import serializers
 
+from store.models import Collection, Product
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=19, decimal_places=2)
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        # fields = '__all__'
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(
+    #     max_digits=19, decimal_places=2, source='unit_price')
+    # # collection = serializers.StringRelatedField()
+    # # collection = CollectionSerializer()
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        tax = product.unit_price * Decimal(1.1)
+        return round(tax, 2)
--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list, name='product-list'),
     path('products/<int:id>', views.product_detail, name='product-detail'),
+    path('collections/<int:pk>', views.collection_detail, name='collection-detail'),
 ]
