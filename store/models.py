52be5b3b581991c7a78bcb91d73c1d312a655872
Karan Mahato <44karanmahato@gmail.com>
2023-07-14 23:31:02 +0545

    cleanup mess

--- store/models.py
+++ store/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
-    discount = models.FloatField()
+    discount = models.FloatField(validators=[MinValueValidator(0)])
 
     def __str__(self):
         return self.description
@@ -12,8 +12,7 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
     def __str__(self):
         return self.title
@@ -23,27 +22,13 @@ class Collection(models.Model):
 
 
 class Product(models.Model):
-    # sku = models.CharField(max_length=255, primary_key=True)
-    title = models.CharField(max_length=255, blank=False, null=False)
+    title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField(blank=True, null=True)
-    unit_price = models.DecimalField(
-        max_digits=19,
-        decimal_places=2,
-        blank=False,
-        null=False,
-        validators=[MinValueValidator(1)])
-
-    inventory = models.IntegerField(
-        blank=False,
-        null=False,
-        validators=[MinValueValidator(1)])
-
+    description = models.TextField(blank=True)
+    unit_price = models.DecimalField(max_digits=19, decimal_places=2, validators=[MinValueValidator(1)])
+    inventory = models.IntegerField(validators=[MinValueValidator(1)])
     last_update = models.DateTimeField(auto_now=True)
-    collection = models.ForeignKey(
-        Collection, on_delete=models.PROTECT, related_name='products')
-    # collection = models.ForeignKey('Collection', on_delete=models.CASCADE) - pass a string model if the model to foreign key to is below/parent class can't be on top
-    # product_set - default reverse relation but we can use sth like 'products' with related_name
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT, related_name='products')
     promotions = models.ManyToManyField(Promotion, blank=True)
 
     def __str__(self):
@@ -58,18 +43,13 @@ class Customer(models.Model):
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_BRONZE = 'B'
 
-    MEMBERSHIP_CHOICES = [
-        (MEMBERSHIP_GOLD, 'Gold'),
-        (MEMBERSHIP_SILVER, 'Silver'),
-        (MEMBERSHIP_BRONZE, 'Bronze')
-    ]
-    first_name = models.CharField(max_length=225, blank=False, null=False)
-    last_name = models.CharField(max_length=225, blank=False, null=False)
+    MEMBERSHIP_CHOICES = [(MEMBERSHIP_GOLD, 'Gold'), (MEMBERSHIP_SILVER, 'Silver'), (MEMBERSHIP_BRONZE, 'Bronze')]
+    first_name = models.CharField(max_length=225)
+    last_name = models.CharField(max_length=225)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=False, null=False)
+    phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True)
-    membership = models.CharField(
-        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
@@ -83,34 +63,17 @@ class Order(models.Model):
     PAYMENT_STATUS_COMPLETE = 'C'
     PAYMENT_STATUS_FAILED = 'F'
 
-    PAYMENT_STAUS_CHOICES = [
-        (PAYMENT_STATUS_PENDING, 'Pending'),
-        (PAYMENT_STATUS_COMPLETE, 'Complete'),
-        (PAYMENT_STATUS_FAILED, 'Failed')
-    ]
-
+    PAYMENT_STAUS_CHOICES = [(PAYMENT_STATUS_PENDING, 'Pending'), (PAYMENT_STATUS_COMPLETE, 'Complete'), (PAYMENT_STATUS_FAILED, 'Failed')]
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STAUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField(
-        max_length=1, choices=PAYMENT_STAUS_CHOICES, default=PAYMENT_STATUS_PENDING)
-
-    customer = models.ForeignKey(
-        Customer, on_delete=models.PROTECT)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
 class OrderItem(models.Model):
+    unit_price = models.DecimalField(max_digits=19, decimal_places=2, validators=[MinValueValidator(1)])
+    quantity = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)])
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(
-        Product, on_delete=models.PROTECT, related_name='orderitems')
-    quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=19, decimal_places=2)
-
-
-class Address(models.Model):
-    street = models.CharField(max_length=255)
-    city = models.CharField(max_length=255, null=False, blank=False)
-    customer = models.OneToOneField(
-        Customer, on_delete=models.CASCADE, primary_key=True)
-    zip = models.CharField(max_length=10, null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
 
 
 class Cart(models.Model):
@@ -118,6 +81,6 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
+    quantity = models.PositiveSmallIntegerField()
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField()
--- storefront/settings.py
+++ storefront/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront2',
+        'NAME': 'storefront_drf',
         'HOST': 'localhost',
         'USER': 'kyto',
         'PASSWORD': 'nice'
